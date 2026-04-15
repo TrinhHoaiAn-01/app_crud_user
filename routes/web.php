164cddd7c5ee836365e19989109a2d2371b81412
Trinh Hoai An <trinhhoaia03@gmail.com>
2026-04-15 15:37:43 +0700

    luu cap nhat user va bo js gia lap

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ Route::post('/register', [CrudUserController::class, 'storeRegister'])->name('re
 Route::get('/list', [CrudUserController::class, 'list'])->name('users.list');
 Route::get('/view', [CrudUserController::class, 'view'])->name('users.view');
 Route::get('/update', [CrudUserController::class, 'edit'])->name('users.edit');
+Route::post('/update', [CrudUserController::class, 'storeUpdate'])->name('users.update');
 Route::get('/delete', [CrudUserController::class, 'delete'])->name('users.delete');
 Route::get('/logout', [CrudUserController::class, 'logout'])->name('logout');
